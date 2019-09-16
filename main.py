70962a3bd696f8d1e7a7bc333e481ba1bf86f8c9
Daniel Coady <daniel.coady@outlook.com.au>
2019-09-16 15:29:10 +1000

    will now tell you which wall you need to 'break' in order to solve maze

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from search import solve
+from copy import deepcopy
 
 '''
 just to get the ol maze we wanna be solving
@@ -12,19 +13,37 @@ def getMaze():
         ["1", "1", "1", "1", "X", "1"],
         ["1", "0", "1", "1", "0", "1"],
         ["1", "0", "0", "0", "0", "1"],
-        ["1", "1", "1", "0", "1", "1"],
+        ["1", "1", "1", "1", "1", "1"],
         ["1", "0", "0", "0", "0", "1"],
         ["1", "O", "1", "1", "1", "1"]
     ]
 
     return maze
 
+def printMaze(maze):
+    print('----')
+    for y in maze:
+        line = ''
+        for x in y:
+            line += str(x)
+        print(line)
+    print('----')
+
 def main():
     maze = getMaze()
-    if solve(maze):
-        print('maze is solvable')
-    else:
-        print('maze is not solvable')
+    width = len(maze[0])
+    fulllen = len(maze) * len(maze[0])
+
+    for i in range(fulllen):
+        x = i % width
+        y = int(i / width)
+        if maze[y][x] == '1':
+            mazecopy = deepcopy(maze)
+            mazecopy[y][x] = 0
+            if solve(mazecopy):
+                print('remove wall at index ', str(y), ', ', str(x))
+                printMaze(mazecopy)
+                break
 
 if __name__ == '__main__':
     main()
